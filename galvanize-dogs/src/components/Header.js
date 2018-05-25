21d9c4f1a5d196e97a82ed513f914f6d78bbe450
missmeow71 <sensuousdesigner@gmail.com>
2018-05-25 10:18:46 -0600

    added content to components

---
+++ galvanize-dogs/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+//import logo from '../g-dino.png';
+
+class App extends Component {
+    render() {
+        return (
+            //<header className="App-header">
+                //<img src={logo} className="App-logo" alt="Dinosaur logo" />
+            //</header>
+         );
+    }
+}
+
+
+
+
+
+
+export default Header;
