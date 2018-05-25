df87ba9c1705af17c968dbb44409e44a798ea2d0
missmeow71 <sensuousdesigner@gmail.com>
2018-05-25 14:40:15 -0600

    added logo

--- galvanize-dogs/src/components/Header.js
+++ galvanize-dogs/src/components/Header.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import logo from '../logo.png';
+
 
 class Header extends Component {
     render() {
         return (
             <header className="App-header">
-                <img src="logo" alt=""/>
+                <img src="./logo.png" id='logo' alt=""/>
             </header>
          );
     }
